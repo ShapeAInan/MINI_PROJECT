93dbad2baece4d2a55bfd55814f08680bf8a617f
Mereena-200 <131443290+Mereena-200@users.noreply.github.com>
2023-04-21 14:39:04 -0400

    Update urls.py

--- backend/app1/urls.py
+++ backend/app1/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 
 urlpatterns = [
-     path('/api/v1/', include('djoser.urls')),
-     path('/api/v1/', include('djoser.urls.jwt'))
+     path('api/v1/', include('djoser.urls')),
+     path('api/v1/', include('djoser.urls.jwt'))
 ]
